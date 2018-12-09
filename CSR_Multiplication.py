f3da576ad00f433ce00ce368b842ce0e103f2a7b
Rohit Jagannath <rohitj559@gmail.com>
2018-12-08 22:00:09 -0600

    Update CSR_Multiplication.py

--- CSR_Multiplication.py
+++ CSR_Multiplication.py
@@ -6,51 +6,73 @@ Created on Sat Dec  8 15:02:23 2018
 """
 import numpy as np
 
-# Scenario 1: 
-a = np.matrix('1 1 0 0 0 0 0 0 ; 1 1 1 1 1 1 0 0 ; 0 0 0 2 1 0 2 1')
-b = np.matrix('1 1 1 1 1 1 1 1 ').T
+# Normal multiplication with '*' function 
+def python_multiplication(a, b):
+    return a*b
 
-ab = a*b
-
-rowSum = 0
-result = []
-for i in range(a.shape[0]):
-    for j in range(b.shape[0]):
-        rowSum += a[i,j]*b[j]
-    result.append(rowSum)
+def loop_multiplication(a, b):
     rowSum = 0
-
-I = [0,1,0,1,2,3,4,5,3,4,6,7]
-V = [1,1,1,1,1,1,1,1,2,1,2,1]
-P = [0,2,8,12]
-
-rowCSR_Sum = 0
-resultCSR = []
-for i in range(len(P)-1):
-    pointerList = list(range(P[i], P[i+1]))    
-    for j in pointerList:
-        rowCSR_Sum += V[j]*b[I[j]]
-    resultCSR.append(rowCSR_Sum)
-    rowCSR_Sum = 0
- 
-# Scenario 2: 
-a = np.matrix('1 1 0 0 0 0 0 0 2; 1 1 1 1 1 1 0 0 2; 0 0 0 2 1 0 2 1 2')
-b = np.matrix('1 1 1 1 1 1 1 1 2').T
-
-I = [0,1,8,0,1,2,3,4,5,8,3,4,6,7,8]
-V = [1,1,2,1,1,1,1,1,1,2,2,1,2,1,2]
-P = [0,3,10,15]
-
-# Scenario 2:
-rowCSR_Sum = 0
-resultCSR = []
-for i in range(len(P)-1):
-    pointerList = list(range(P[i], P[i+1]))    
-    for j in pointerList:
-        rowCSR_Sum += V[j]*b[I[j]]
-    resultCSR.append(rowCSR_Sum)
-    rowCSR_Sum = 0
+    result = []
+    for i in range(a.shape[0]):
+        for j in range(b.shape[0]):
+            rowSum += a[i,j]*b[j]
+        result.append(rowSum)
+        rowSum = 0
+    return result
         
+def csr_Multiplication(a, b, I, V, P):
+    rowCSR_Sum = 0
+    resultCSR = []
+    for i in range(len(P)-1):
+        pointerList = list(range(P[i], P[i+1]))    
+        for j in pointerList:
+            rowCSR_Sum += V[j]*b[I[j]]
+        resultCSR.append(rowCSR_Sum)
+        rowCSR_Sum = 0
+    return resultCSR
+
+# =============================================================================
+# # Testing scenario 1: 
+# =============================================================================
+input_1 = np.matrix('1 1 0 0 0 0 0 0 ; 1 1 1 1 1 1 0 0 ; 0 0 0 2 1 0 2 1')
+input_2 = np.matrix('1 1 1 1 1 1 1 1 ').T
+
+Index1 = [0,1,0,1,2,3,4,5,3,4,6,7]
+Value1 = [1,1,1,1,1,1,1,1,2,1,2,1]
+Pointer1 = [0,2,8,12]
+
+pythonProduct1 = python_multiplication(input_1, input_2)
+
+loopProduct1 = loop_multiplication(input_1, input_2)
+
+csr_Product1 = csr_Multiplication(input_1, input_2, Index1, Value1, Pointer1)
+
+# =============================================================================
+# # Testing Scenario 2:
+# =============================================================================
+input_3 = np.matrix('1 1 0 0 0 0 0 0 2; 1 1 1 1 1 1 0 0 2; 0 0 0 2 1 0 2 1 2')
+input_4 = np.matrix('1 1 1 1 1 1 1 1 2').T
+
+Index2 = [0,1,8,0,1,2,3,4,5,8,3,4,6,7,8]
+Value2 = [1,1,2,1,1,1,1,1,1,2,2,1,2,1,2]
+Pointer2 = [0,3,10,15]
+
+pythonProduct2 = python_multiplication(input_3, input_4)
+
+loopProduct2 = loop_multiplication(input_3, input_4)
+
+csr_Product2 = csr_Multiplication(input_3, input_4, Index2, Value2, Pointer2)
+    
+
+ # =============================================================================
+# rowSum = 0
+# result = []
+# for i in range(a.shape[0]):
+#     for j in range(b.shape[0]):
+#         rowSum += a[i,j]*b[j]
+#     result.append(rowSum)
+#     rowSum = 0
+# =============================================================================       
   
 #pointerDict = dict(zip(list(range(P[i], P[i+1])), list(range(P[i+1] - P[i]))))     
     
